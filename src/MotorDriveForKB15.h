6584d2a86d32439598af82d518028a24ae9912d0
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2020-08-02 21:02:08 +0700

    Add files via upload

--- src/MotorDriveForKB15.h
+++ src/MotorDriveForKB15.h
@@ -8,22 +8,6 @@
 #define M2A 18
 #define M2B 21
 
-
-void MotorDriveForKB15_Begin(){
-  pinMode(M1A,OUTPUT);
-  pinMode(M1B,OUTPUT);
-  pinMode(M2A,OUTPUT);
-  pinMode(M2B,OUTPUT);
-  ledcSetup(6, 5000, 8);
-  ledcSetup(7, 5000, 8);
-  ledcSetup(4, 5000, 8);
-  ledcSetup(5, 5000, 8);
-  ledcAttachPin(M1A, 6);
-  ledcAttachPin(M1B, 7);
-  ledcAttachPin(M2A, 4);
-  ledcAttachPin(M2B, 5);
-
-}
 void motor(int pin,int _direction, int _Speeds) {
   int _SpeedsA;
   int _SpeedsB;
@@ -48,12 +32,32 @@ void motor(int pin,int _direction, int _Speeds) {
     if (_SpeedsB > 255){_SpeedsB = 255;}
     else if (_SpeedsB < -255){_SpeedsB = -255;}
     if (_direction == 1) {
-      ledcWrite(4, 0);
-      ledcWrite(5, abs(_SpeedsB));
+      ledcWrite(2, 255-abs(_SpeedsB));
+      ledcWrite(3, 255);
     }
     else if (_direction == 2) {
-      ledcWrite(4, abs(_SpeedsB));
-      ledcWrite(5, 0);
+      ledcWrite(2, 255);
+      ledcWrite(3, 255-abs(_SpeedsB));
     }
   }
 }
+void MotorDriveForKB15_Begin(){
+  pinMode(M1A,OUTPUT);
+  pinMode(M1B,OUTPUT);
+  pinMode(M2A,OUTPUT);
+  pinMode(M2B,OUTPUT);
+  ledcSetup(6, 5000, 8);
+  ledcSetup(7, 5000, 8);
+  ledcSetup(2, 5000, 8);
+  ledcSetup(3, 5000, 8);
+  ledcAttachPin(M1A, 6);
+  ledcAttachPin(M1B, 7);
+  ledcAttachPin(M2A, 2);
+  ledcAttachPin(M2B, 3);
+  ledcWrite(6, 255);
+  ledcWrite(7, 255);
+  ledcWrite(2, 255);
+  ledcWrite(3, 255);
+
+}
+
